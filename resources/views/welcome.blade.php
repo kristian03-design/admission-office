8101f0c3759c62610591453f98bb7c32c09d3cd8
kristian03-design <hkristianlloyd2@gmail.com>
2026-05-27 01:53:45 +0800

    style(navbar): enhance desktop navigation layout and visibility
    - Added CSS rules for desktop navbar links to improve layout and spacing.
    - Ensured visibility and alignment of navbar elements on larger screens.
    - Incremented stylesheet version for home-page.css to reflect recent changes.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
   <!-- ✦ Iconsax Icons ✦ -->
   @include('partials.iconsax')
 
-  <link rel="stylesheet" href="{{ asset('css/home-page.css') }}?v=43" />
+  <link rel="stylesheet" href="{{ asset('css/home-page.css') }}?v=44" />
 
 </head>
 
